96bda8244e6e2ed9ae15a176153f09227b8d9baa
Ethan Mulcahy <ethanmulcahy@gmail.com>
2024-02-15 13:04:08 -0500

    Add inheritance and change factory method

--- csv_parser.py
+++ csv_parser.py
@@ -1,21 +1,40 @@
 import pandas as pd
 
-class CSVParser:
-    def parser_factory(self, args):
-        self._unparsed_data = pd.read_csv(args.filename) # csv as a 2d array
+# Factory Method (https://www.geeksforgeeks.org/factory-method-python-design-patterns/)
+def CSVParserFactory(key_orientation='top'):
+    parsers = {
+        'left':_LeftParser,
+        'top': _TopParser,
+    }
+    return parsers[key_orientation]()
+
+# Base class, not to be instantiated
+class _CSVParser():
+    def parse(self, filename):
+        if __class__ == "CSVParser":
+            raise NotImplementedError
+        
+        self._unparsed_data = pd.read_csv(filename) # csv as a 2d array
         self._parsed_data = {}
-        if args.left:
-            self._parse_left()
-        else:
-            self._parse_top()
+
+    def _to_json(self):
+        if __class__ == "CSVParser":
+            raise NotImplementedError
+        
+        return self._parsed_data.to_json()
+
+# Parser for left keys
+class _LeftParser(_CSVParser):
+    def parse(self, filename):
+        super().parse(filename)
+        self._parsed_data = self._unparsed_data.transpose().copy()
         
         return self._to_json()
 
-    def _parse_left(self):
-        self._parsed_data = self._unparsed_data.transpose().copy()
-
-    def _parse_top(self):
+# Parser for top keys
+class _TopParser(_CSVParser):
+    def parse(self, filename):
+        super().parse(filename)
         self._parsed_data = self._unparsed_data.copy()
-
-    def _to_json(self):
-        return self._parsed_data.to_json()
+        
+        return self._to_json()
--- main.py
+++ main.py
@@ -7,23 +7,27 @@ def main():
     args = parse_args()
 
     # Create a new CSVParser()
-    parser = csv_parser.CSVParser()
+    if args.top:
+        parser = csv_parser.CSVParserFactory('top')
+    if args.left:
+        parser = csv_parser.CSVParserFactory('left')
+    else:
+        parser = csv_parser.CSVParserFactory()
 
-    # get json
-    json = parser.parser_factory(args)
-    parsed = js.loads(json) 
-    
+    # get json (https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_json.html)
+    json = parser.parse(args.filename)
+    parsed_json = js.loads(json) 
 
     # write json to output.txt
     with open("output.txt", "w") as out_file:
-        out_file.write(js.dumps(parsed, indent=4) )
+        out_file.write(js.dumps(parsed_json, indent=4) )
 
 def parse_args():
     parser = ap.ArgumentParser(
                     prog='csv_parser',
                     description='parse csv to json',
                     epilog='Created by: Ethan Mulcahy & Jonathan Pimentel')
-    
+
     # arguments
     parser.add_argument('filename')
     parser.add_argument('-t', '--top', action='store_true', required=False, help='keys are located on top row (default)')
